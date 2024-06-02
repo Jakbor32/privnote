3edc4953970ed74d5b327b74de438af9a9bebf52
Jakub <borowyjakub32@gmail.com>
2024-06-02 23:05:25 +0200

    add secure link env file using vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+  return {
+    // vite config
+    define: {
+      __APP_ENV__: process.env.VITE_VERCEL_ENV,
+    },
+    plugins:   [react()],
+  }
 })
+
+
+
+ 
+  
+  
